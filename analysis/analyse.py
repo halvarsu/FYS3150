a79a3188f9c770114316b09c12e015975dc4666f
Halvard Sutterud <halvarsu@oscilloskop.uio.no>
2017-09-21 16:58:24 +0200

    add basic analysis

---
+++ analysis/analyse.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+file_dir = "../build-cpp_proj2-Desktop_Qt_5_9_1_GCC_64bit-Debug/"
+
+eigval = np.loadtxt(file_dir + "eigval.txt",skiprows=2)
+eigvec = np.loadtxt(file_dir + "eigvec.txt",skiprows=2)
+r = np.loadtxt(file_dir + "radial_val.txt",skiprows=2)
+
+plt.plot(r,eigvec[2]**2)
+plt.show()

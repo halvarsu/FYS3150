f3664a1828b43941db26a85f0c901de2e64d1691
ragnarbru <ragnar.bruvoll@gmail.com>
2017-10-06 16:54:23 +0200

    some changes

--- projects/Project2/python/comp_pro2_1.py
+++ projects/Project2/python/comp_pro2_1.py
@@ -1,26 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.constants import hbar
 import sys
 
-N  = 5
-k = 4
+N  = 100
 
 # defining step size
 rho_start = 0
 rho_end = 5
 h = float(rho_end-rho_start)/N
 
-hbar = 1
+omega = 1
 
 # making matrix A
 A = np.zeros((N,N))
 V = np.zeros(N)
 for i in range(N):
-	rho = i*h
-	V[i] = .5*k*rho**2
-	d = 2.0/hbar**2 + V[i]
-	e = -1.0/hbar**2
+	rho = (i+1)*h
+	V[i] = rho**2*omega**2
+#	V[i] = rho**2+1/rho
+	d = 2.0/h**2 + V[i]
+	e = -1.0/h**2
 # inserting the three diagonal element series
 	A[i,i] = d
 	if i > 0: 
@@ -36,12 +35,10 @@ def find_largest(A,N):
 		for j, value in enumerate(A[i,i+1:]):
 			if abs(value) > largest:
 				largest = abs(value)
-				k,l=i,j+i+1			
+				k,l=i,j+i+1	
 	return largest,k,l
 
 # finding sin and cos
-k = find_largest(A,N)[1]
-l = find_largest(A,N)[2]
 def s_c(A,k,l):
 	tau = (A[l,l]-A[k,k])/float((2.0*A[k,l]))
 	if(tau >= 0 ):
@@ -52,50 +49,55 @@ def s_c(A,k,l):
 	s = t*c
 	return s,c
 
-# making matrix B
-B = np.copy(A)
-s = s_c(A,k,l)[0]
-c = s_c(A,k,l)[1]
-B[k,k] = A[k,k]*c**2-2*A[k,l]*c*s+A[l,l]*s**2
-B[l,l] = A[l,l]*c**2+2*A[k,l]*c*s+A[k,k]*s**2
-B[k,l] = 0 #(A[k,k]-A[l,l])*c*s + A[k,l]*(c**2-s**2)
-B[l,k] = 0 
-for i in range(N):
-	if i != k and i!= l:
-		B[i,k] = A[i,k]*c - A[i,l]*s
-		B[k,i] = B[i,k]
-		B[i,l] = A[i,l]*c - A[i,k]*s
-		B[l,i] = B[i,l]
-#print B
 
-
+# making diagonal matrix B from A
 def matB(A,N,n):
 	B = np.copy(A)
-	for i in range(n):
-		_, k, l = find_largest(B,N)
-
-		s = s_c(B,k,l)[0]
-		c = s_c(B,k,l)[1]
-		B[k,k] = B[k,k]*c**2-2*B[k,l]*c*s+B[l,l]*s**2
-		B[l,l] = B[l,l]*c**2+2*B[k,l]*c*s+B[k,k]*s**2
-		B[k,l] = 0 #(A[k,k]-A[l,l])*c*s + A[k,l]*(c**2-s**2)
+	for j in range(n):
+		largest, k, l = find_largest(B,N)
+		if abs(largest) < 1e-8:
+			print "exit",j
+			return np.sort(np.diagonal(B))
+			
+		s,c = s_c(B,k,l)
+		b_kk, b_ll = B[k,k], B[l,l]
+		B[k,k] = b_kk*c**2-2*B[k,l]*c*s+b_ll*s**2
+		B[l,l] = b_ll*c**2+2*B[k,l]*c*s+b_kk*s**2
+		B[k,l] = 0 
 		B[l,k] = 0 
 		for i in range(N):
 			if i != k and i!= l:
-				B[i,k] = B[i,k]*c - B[i,l]*s
+				b_il, b_ik = B[i,l], B[i,k]
+				B[i,k] = b_ik*c - b_il*s
 				B[k,i] = B[i,k]
-				B[i,l] = B[i,l]*c - B[i,k]*s
+				B[i,l] = b_il*c + b_ik*s
 				B[l,i] = B[i,l]
-	return B
+				
+	return np.sort(np.diagonal(B))
 
-print matB(A,N,40)	
+sorted_eig_B = matB(A,N,100000)
 
+# my calculated eigenvalues
+print sorted_eig_B
 
+# letting numpy calculate values
+eig,eigv = np.linalg.eig(A)		
+print np.sort(eig)
 
+# testing calculated eigenvalues to exact ones
+B_eig = np.copy(sorted_eig_B)
+def testone(B,A):
+	eig,eigv = np.linalg.eig(A)
+	calcu = B
+	exact = np.sort(eig)
+	tol = 1e-5
+	for i in range(N):
+		if np.abs(float(calcu[i]-exact[i])) > tol:
+			print "Eigenvalue number ",i+1," has a larger error than",tol,". The error is ",np.abs(float(calcu[i]-exact[i]))
+		
+B_eig[0] = 4
+testone(B_eig,A)
 
-
-
-
-
-
+plt.plot(eigv)
+plt.show()
 

1a03c5404a1eaec523bcee7ceacb60d4397f1bd0
halvarsu <halvard.sutterud@gmail.com>
2017-10-19 10:50:04 +0200

    update analysis files

--- projects/Project3/analyse/analyse.py
+++ projects/Project3/analyse/analyse.py
@@ -2,13 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-pos = np.loadtxt('out/pos.txt', skiprows=2, dtype=float)
-vel = np.loadtxt('out/vel.txt', skiprows=2, dtype=float)
-plt.scatter(0,0, c='y')
-plt.axis('equal')
+x0=1.563246546807631E+00 
+y0=5.725226790391031E-01 
+z0=5.016658374850980E-02
+vx0=-4.244787458165965E-03 
+vy0=-1.195685962735690E-02
+vz0=-1.465083647294641E-04
 
-print(pos.shape)
 
-#plt.plot(pos[:,0],pos[:,1])
-plt.plot(vel[:,0],vel[:,1])
+
+filename = 'out/test.txt'
+
+with open(filename, 'r') as infile:
+    n_planets = int(infile.readline())
+
+pos = np.loadtxt(filename, skiprows=1, dtype=float)
+planets = np.zeros((pos.shape[0]/n_planets, n_planets, pos.shape[1]))
+for i in range(n_planets):
+    planets[:,i] = pos[i::n_planets]
+
+plt.plot(planets[:,:,1],planets[:,:,0], '-o', markersize =3)
 plt.show()

d1453ef87b43e65c8b3a42bbed48bbc01925bbae
ragnarbru <ragnar.bruvoll@gmail.com>
2017-10-24 14:35:08 +0200

    corrections to minpoint-code

--- projects/Project3/analyse/minpoints.py
+++ projects/Project3/analyse/minpoints.py
@@ -2,47 +2,40 @@ import random as ran
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-# making random elliptic orbit
+# making random ellipticish orbit
 N = 1000
-r = np.zeros((2,N))
+r = np.zeros((3,N))
 t = np.linspace(0,2*np.pi,N)
 for i in range(N):
 	r[0,i] = np.sin(t[i])
 	r[1,i] = ran.uniform(.4,.6)*np.cos(t[i])
 	
-
 # saving smallest distances and the smallest distancce index
-list_of_min_points = []
 min_dist = 1e24
 for i in range(N):
 	dist = np.linalg.norm(r[:,i])
 	if dist < min_dist:
 		min_dist = dist
-		list_of_min_points.append(r[:,i])
-		index_smallest = i
-		print min_dist, i
-print list_of_min_points[:]
-i_s = index_smallest
+		min_x,min_y,min_z = r[0,i],r[1,i],r[2,i]
+		ind = i # index for 
 
 # from radians to degrees with correct +/-
-theta =np.arccos(r[0,i_s]/min_dist)*360/(2*np.pi)
-print theta
-if r[0,i_s] < 0 and r[1,i_s] > 0:
-	print "-x  +y"
+theta =np.arccos(r[0,ind]/min_dist)*360/(2*np.pi)
+if r[0,ind] < 0 and r[1,ind] > 0:
 	theta = theta
-elif r[0,i_s] < 0 and r[1,i_s] < 0:
-	print "-x  -y"
+elif r[0,ind] < 0 and r[1,ind] < 0:
 	theta = 360-theta
-elif r[0,i_s] > 0 and r[1,i_s] < 0:
-	print "+x  -y"
+elif r[0,ind] > 0 and r[1,ind] < 0:
 	theta = 360-theta
 else:
 	theta = theta
 
-print theta,"degrees"
-
-x, y = [0, r[0,i_s]], [0, r[1,i_s]]
+print "angle is ",theta,"degrees"
+print "coordinates are"
+print "x:",min_x
+print "y:",min_y
+print "z:",min_z
+x, y = [0, r[0,ind]], [0, r[1,ind]]
 plt.plot(x, y, marker = 'o')
 plt.legend([theta,"degrees"])
 plt.plot(r[0,:],r[1,:])

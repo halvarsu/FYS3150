2f52496745d72c9160da644303cfbf1989316e60
halvarsu <halvard.sutterud@gmail.com>
2017-10-25 23:55:30 +0200

    read binary files

--- projects/Project3/analyse/analyse.py
+++ projects/Project3/analyse/analyse.py
@@ -5,40 +5,60 @@ from mpl_toolkits.mplot3d import Axes3D
 def get_args():
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--filename', default='out/example.txt')
+    parser.add_argument('-f', '--filename', default='out/example.bin')
     parser.add_argument('--fixed_sun', action='store_true')
     parser.add_argument('-dim', '--dimensions',type=int,help='plot dimension', default=2, choices=[2,3])
-    parser.add_argument('-s', '--plot_step', help='plot every nth data point',
+    parser.add_argument('-ps', '--plot_step', help='plot every nth data point',
             type=int,default=1)
     parser.add_argument('-p', '--precession', help='plot perihelion precession of body 2',
             action = 'store_true')
+    parser.add_argument('-e','--energies', help='plot energies of system',action='store_true')
     return parser.parse_args()
 
 def read_data(args):
     # Finds number of planets from first line, and reads the rest of the
     # data. 
-    filename = args.filename
-    print("Reading %s" %filename)
-    with open(filename, 'r') as infile:
-        n_planets = int(infile.readline())
+    data_filename = args.filename
+    temp = data_filename.split('.')[:-1]
+    temp.append("info.txt")
+    info_filename = ".".join(temp)
+    data = {}
 
-    pos = np.loadtxt(filename, skiprows=1, dtype=float)
+    print("Reading info from %s" %info_filename)
+    with open(info_filename, 'r') as infile:
+        data["n_planets"] = int(infile.readline())
+        data["fixed_sun"] = int(infile.readline())
+        data["relativistic"] = int(infile.readline())
+        _, _, _ = infile.readline().split() # headers for energies, not necessary
+        # in this context
 
-    planets = np.zeros((pos.shape[0]//n_planets, n_planets, pos.shape[1]))
-    for i in range(n_planets):
-        planets[:,i] = pos[i::n_planets]
-    return n_planets, planets
+    if args.energies:
+        data["energies"] = np.loadtxt(info_filename, skiprows=4, dtype=float)
 
-def plot(planets, args):
-    print(planets.shape)
-    n_planets = planets.shape[1]
+    print("Reading pos from %s" %data_filename)
+    pos = np.fromfile(data_filename)
+    pos = pos.reshape(-1, data["n_planets"], 3)
+    data["pos"] = pos
+
+    #planets = np.zeros((pos.shape[0]//n_planets, n_planets, pos.shape[1]))
+    #for i in range(n_planets):
+        #planets[:,i] = pos[i::n_planets]
+    print("\r    Done reading...     ")
+    print(pos)
+    return data
+
+def plot(data, args):
+    print(data['pos'].shape)
+    n_planets = data["n_planets"]
+    planets = data["pos"]
     fig = plt.figure()
-    n = args.plot_step
+    n_step = args.plot_step
 
     if args.dimensions == 3:
         ax = fig.add_subplot(111, projection='3d')
         for i in range(n_planets):
-            ax.plot(planets[::n,i,0], planets[::n,i,1], zs=planets[::n,i,2])
+            ax.plot(planets[::n_step,i,0], planets[::n_step,i,1],
+                    zs=planets[::n_step,i,2])
         m = np.max(np.abs(planets))
         boxlen = [-m,m]
         plt.scatter(planets[0,0,0],planets[0,0,1],zs=planets[0,0,2], c='y')
@@ -46,7 +66,7 @@ def plot(planets, args):
     else:
         ax = fig.add_subplot(111)
         for i in range(n_planets):
-            ax.plot(planets[::n,i,0], planets[::n,i,1])
+            ax.plot(planets[::n_step,i,0], planets[::n_step,i,1])
         plt.scatter(planets[0,0,0],planets[0,0,1], c='y')
         ax.axis('equal')
         ax.grid()
@@ -75,7 +95,7 @@ def plot_peri_precession(planet,args):
 
 def main():
     args = get_args()
-    n_planets, data = read_data(args)
+    data = read_data(args)
     plot(data,args)
 
 
--- projects/Project3/analyse/read_horizon.py
+++ projects/Project3/analyse/read_horizon.py
@@ -1,5 +1,17 @@
 import sys
 import re
+import numpy as np
+
+def get_mass(body_name):
+    sun_mass = 1.988544e6 # in 10e24 kg
+    pm = {'Mercury':0.3301 ,'Venus':4.867 ,'Earth':5.972 ,'Moon':0.073
+            ,'Mars':0.642 ,"Jupiter":1898
+            ,'Saturn':568 ,'Uranus':86.8 ,'Neptune':102 ,'Pluto':0.0146} # in 10e24 kg
+
+    masses = {k:m/sun_mass for k,m in pm.items()}        
+    masses['Sun'] = 1
+    return masses[body_name]
+
 
 def parse_body_name(text):
     name_index = text.find("Target body name: ")
@@ -23,18 +35,43 @@ def process_data(text):
 
     # pick out target name
     body_name = parse_body_name(text)
-    mass = masses[body_name]
+    mass = get_mass(body_name)
     print("Parsing body: %10s  | Mass: %12g solar masses" %(body_name, mass))
     pos, vel = parse_pos_and_vel(text)
 
     # Convert GM to sun masses 
     if body_name == 'Earth':
-        import numpy as np
         print("earth dist to sun[AU]: %g " %np.linalg.norm(list(map(float, pos))))
         print("earth velocity[AU/yr]: %g " %np.linalg.norm(list(map(float, vel))))
 
     data = " ".join(pos) + " " + " ".join(vel) + " "+ str(mass)
     return body_name, data
+
+def fix_barycentric(outfile_lines):
+    """Reads the data and corrects velocities such that the center of mass
+    is at rest"""
+    # [years, steps_per_year, has_fixed_sun, relativistic, n_bodies]
+    info = outfile_lines[:5]
+    bodies = outfile_lines[5:]
+    bodies = np.array(map(lambda x:map(float,x.split()),bodies))
+    pos = bodies[:,:3]
+    vel =  bodies[:,3:-1]
+    mass = bodies[:,-1]
+    M = np.sum(mass)
+    mom = (vel.T*mass).T
+    tot_mom = np.sum(mom, axis=0)
+    vel =  vel -tot_mom / M
+
+    bodies_array = np.concatenate((pos,vel,mass[:,np.newaxis]),axis=1)
+    print(bodies_array.shape)
+    bodies = []
+    for body in bodies_array:
+        bodies.append(" ".join(map(str,body)))
+
+    outfile_lines_new = info + bodies
+    return outfile_lines_new
+    
+
 
 
 def read_mbox(args):
@@ -59,27 +96,13 @@ def get_args():
     parser.add_argument('-m','--main_body',default='Sun')
     parser.add_argument('-y','--years',default=2, type=int)
     parser.add_argument('-s','--steps_per_year',default=10000, type=int)
+    parser.add_argument('--fix_barycentric',action='store_true')
     parser.add_argument('--fixed_sun',action='store_true')
     parser.add_argument('--relativistic',action='store_true')
     args = parser.parse_args()
     return args
 
 def main(args):
-    return
-
-if __name__ == "__main__":
-    args =  get_args()
-
-    sun_mass = 1.988544e6 # in 10e24 kg
-    pm = {'Mercury':0.3301 ,'Venus':4.867 ,'Earth':5.972 ,'Moon':0.073
-            ,'Mars':0.642 ,"Jupiter":1898
-            ,'Saturn':568 ,'Uranus':86.8 ,'Neptune':102 ,'Pluto':0.0146} # in 10e24 kg
-
-    masses = {k:m/sun_mass for k,m in pm.items()}        
-    masses['Sun'] = 1
-
-
-
     import glob
     if args.folder:
         files = glob.glob(args.folder+'/*')
@@ -108,6 +131,8 @@ if __name__ == "__main__":
             outfile_lines.insert(n_info_lines,data)
         else:
             outfile_lines.append(data)
+    if args.fix_barycentric:
+        outfile_lines = fix_barycentric(outfile_lines)
 
     outfile_text = '\n'.join(map(str, outfile_lines))
 
@@ -133,3 +158,9 @@ if __name__ == "__main__":
         outfile.write(outfile_text)
         print('Success! Wrote %d characters on %d lines to file %s'
                 %(len(data),len(outfile_lines), outfile_name))
+    return
+
+if __name__ == "__main__":
+    args =  get_args()
+    main(args)
+

e6d491dca2a72db567a127efa771cf009fb63409
halvarsu <halvard.sutterud@gmail.com>
2017-10-31 14:51:06 +0100

    init cpp

---
+++ projects/Project4/cpp/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <armadillo>
+#include <chrono>
+#include <iomanip>
+
+using namespace std;
+
+
+int main(int argc, char * argv[]) {
+    cout << "TEST" << endl;
+}

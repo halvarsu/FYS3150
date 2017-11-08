c434c331b6ec06deee22cb949075a144295c1822
Halvard Sutterud <halvard.sutterud@gmail.com>
2017-11-08 15:39:54 +0100

    template for readData

--- projects/Project4/cpp/tools.cpp
+++ projects/Project4/cpp/tools.cpp
@@ -1,6 +1,9 @@
 #include <random>
 #include "tools.h"
 
+void readData(string filename, int& NMC, double & T, int &L, bool parallel, bool timeit, bool save_to_file){
+    return;
+}
 
 int periodic(int i, int limit, int add)
 {

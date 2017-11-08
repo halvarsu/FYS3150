01c8ebe7b86b1c795bc13561e5a29bdca04f98ce
Halvard Sutterud <halvard.sutterud@gmail.com>
2017-11-08 16:44:04 +0100

    added argument

--- projects/Project4/cpp/main.cpp
+++ projects/Project4/cpp/main.cpp
@@ -10,40 +10,47 @@
 using namespace std;
 
 
+int main(int argc, char * argv[]) {
+    int L = 2;
+    int NMC;
+    double T;
 
-int main(int soetpotet, char * argv[]) {
-    int N = 2;
-    int NMC = 1000000;
-
-    if(soetpotet == 1){
+    if(argc < 2){
         cout << "Give me your number of montecarlo simulations!" << endl;
         cin >> NMC;
     } else {
         NMC = atoi(argv[1]);
     }
 
-//    std::random_device rd;
-//    std::mt19937 gen(rd());
-//    std::uniform_real_distribution<> dist(0.,1.);
-//    cout << dist(gen) << endl;
+    if(argc < 3){
+        cout << "Give me a temperature!" << endl;
+        cin >> T;
+    } else {
+        T = atof(argv[2]);
+    }
+
+    //    std::random_device rd;
+    //    std::mt19937 gen(rd());
+    //    std::uniform_real_distribution<> dist(0.,1.);
+    //    cout << dist(gen) << endl;
 
     // int seed = 0;
-    MetropolisSolver solver(N);//, seed);
+    MetropolisSolver solver(L);//, seed);
 
     // Generate spin matrix with random values of either -1 or 1:
-    arma::mat spin_matrix = 2*arma::randi<arma::mat>(N,N,arma::distr_param(0,1)) - 1;
-    // arma::mat spin_matrix = - arma::ones<arma::mat>(N,N);
+    arma::mat spin_matrix = 2*arma::randi<arma::mat>(L,L,arma::distr_param(0,1)) - 1;
+    // arma::mat spin_matrix = - arma::ones<arma::mat>(L,L);
 
     // handling boundry conditions
     double E = 0;
 
-    for (int i=0; i<N; i++){
-        for (int j=0; j<N; j++){
+    for (int i=0; i<L; i++){
+        for (int j=0; j<L; j++){
             E += - spin_matrix(i,j)*
-                (spin_matrix(i,periodic(j,N,1))+
-                 spin_matrix(periodic(i,N,1),j));//+
-                 //spin_matrix(i,periodic(j,N,-1))+
-                 //spin_matrix(periodic(i,N,-1),j));
+                (spin_matrix(i,periodic(j,L,1))+
+                 spin_matrix(periodic(i,L,1),j));//+
+                 //spin_matrix(i,periodic(j,L,-1))+
+                 //spin_matrix(periodic(i,L,-1),j));
         }
     }
 
@@ -55,7 +62,7 @@ int main(int soetpotet, char * argv[]) {
     arma::vec w;
     deltaE << -8 << -4 << 0 << 4 << 8;
 
-    double T = 1.0;
+    // double T = 1.0;
     double beta = 1./T;
     w = arma::exp(- beta * deltaE);
 

ab3bafb8d1f4294f70241ad8c2f0da86305062b8
Halvard Sutterud <halvard.sutterud@gmail.com>
2017-11-10 13:41:19 +0100

    MPI test

--- projects/MPI_test/main.cpp
+++ projects/MPI_test/main.cpp
@@ -1,9 +1,17 @@
+//    First C++ example of MPI Hello world
+using namespace std;
+#include <mpi.h>
 #include <iostream>
 
-using namespace std;
-
-int main()
+int main (int nargs, char* args[])
 {
-    cout << "Hello World!" << endl;
-    return 0;
+  int numprocs, my_rank;
+  //   MPI initializations
+  MPI_Init (&nargs, &args);
+  MPI_Comm_size (MPI_COMM_WORLD, &numprocs);
+  MPI_Comm_rank (MPI_COMM_WORLD, &my_rank);
+  cout << "Hello world, I have  rank " << my_rank << " out of " << numprocs << endl;
+  //  End MPI
+  MPI_Finalize ();
+  return 0;
 }

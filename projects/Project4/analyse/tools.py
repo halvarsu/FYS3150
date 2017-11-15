8847699527bb0879a92764edca6bee78fbf811ce
Halvard Sutterud <halvard.sutterud@gmail.com>
2017-11-15 15:36:34 +0100

    move process_data

--- projects/Project4/analyse/tools.py
+++ projects/Project4/analyse/tools.py
@@ -7,3 +7,17 @@ def sine_print(text, i, width=60, freq=10):
 def linear_print(text, i, width, end=100):
     print("{1: ^{0}}".format(width,
         "*{1:=^{0}}*".format(int((width-len(text)-2)*i/end+len(text)),text)))
+
+
+def process_data(data):
+    sim_data = []
+    for simulation in data:
+        line_data = []
+        lines = simulation.split('\n')
+        for line in lines:
+            out = [float(d) for d in line.split()]
+            if out:
+                line_data.append(out)
+        sim_data.append(line_data)
+    return np.array(sim_data).swapaxes(0,1).swapaxes(1,2)
+

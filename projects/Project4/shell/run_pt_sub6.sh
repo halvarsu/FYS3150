05881b73647b254a5ed95ce98c5d1fb403fc6497
halvarsu <halvard.sutterud@gmail.com>
2017-11-16 22:14:15 +0100

    rename executable of mpirun to be able to run via ssh. Wont work on all machines

--- projects/Project4/shell/run_pt_sub6.sh
+++ projects/Project4/shell/run_pt_sub6.sh
@@ -4,6 +4,6 @@ L=40
 
 while [ $L -lt 101 ]; do
     echo "=================== L = $L ========================"
-    mpirun -np 4 ./build/Project4 1000000 2.376884 2.449246 24 $L 1 0 phase_transitions/pt_sub6_L$L verbose
+    /usr/lib64/openmpi/bin/mpirun -np 4 ./build/Project4 1000000 2.376884 2.449246 24 $L 1 0 phase_transitions/pt_sub6_L$L verbose
     let L=$L+20
 done

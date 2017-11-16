1df643c2026e919f16cab36a4467a89ef6456306
Halvard Sutterud <halvard.sutterud@gmail.com>
2017-11-16 13:16:05 +0100

    setup files for calculating lots of energies

---
+++ projects/Project4/run_pt_sub3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+L=40
+
+while [ $L -lt 101 ]; do
+    echo "=================== L = $L ========================"
+    mpirun -np 4 ./build/Project4 1000 2.150754 2.223116 25 $L 1 0 phase_transitions/pt_sub3_L$L verbose
+    let L=$L+20
+done

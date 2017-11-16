b89c772f709a9a3fff49466413172df547935814
halvarsu <halvard.sutterud@gmail.com>
2017-11-16 18:54:31 +0100

    25 -> 24

--- projects/Project4/run_pt_sub8.sh
+++ projects/Project4/run_pt_sub8.sh
@@ -4,6 +4,6 @@ L=40
 
 while [ $L -lt 101 ]; do
     echo "=================== L = $L ========================"
-    mpirun -np 4 ./build/Project4 1000000 2.527638 2.600000 25 $L 1 0 phase_transitions/pt_sub8_L$L verbose
+    mpirun -np 4 ./build/Project4 1000000 2.527638 2.600000 24 $L 1 0 phase_transitions/pt_sub8_L$L verbose
     let L=$L+20
 done

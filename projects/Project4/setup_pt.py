0f08208431c331834a4126f82831447d85a50bb2
halvarsu <halvard.sutterud@gmail.com>
2017-11-17 15:57:43 +0100

    setup pt

--- projects/Project4/setup_pt.py
+++ projects/Project4/setup_pt.py
@@ -1,67 +1,12 @@
 # coding: utf-8
 import numpy as np
-a = np.linspace(2,2.3,100)
-a
-a[1]
-dt = a[1] - a[0]
-a = np.linspace(2+dt/2,2.3+dt/2,100)
-a
-np.linspace(2,2.6,200)
-a = np.linspace(2,2.6,200)
-for i in range(8):
-    b = a[i::8]
-    
-for i in range(8):
-    b = a[i::8]
-    print(b[0])
-    
-for i in range(8):
-    print(a[len(a)/8)]
-    
-    
-    
-for i in range(8):
-    print(a[len(a)/8)])
-    
-    
-    
-    
-for i in range(8):
-    print(a[len(a)/8])
-    
-for i in range(8):
-    print(a[i*len(a)/8])
-    
-for i in range(8):
-    print(a[i*len(a)/8])
-    
-for i in range(8):
-    print(a[i*len(a)/8])
-    print(a[(i+1)*len(a)/8 - 1])
-    
-for i in range(8):
-    print(a[i*len(a)/8], a[(i+1)*len(a)/8 - 1])
+
+a = np.linspace(2.2,2.4,192)
     
 for i in range(8):
     print a[i*len(a)/8], a[(i+1)*len(a)/8 - 1]
-    
-    
-for i in range(8):
-    start, stop = a[i*len(a)/8], a[(i+1)*len(a)/8 - 1]
-    print('mpirun -np 4 ./build/Project4 1000000 %f %f 25 $L 1 0 pt_sub1_L$L verbose' %(start, stop))
-    
-    
-    
-
 
     
-    
-for i in range(8):
-    start, stop = a[i*len(a)/8], a[(i+1)*len(a)/8 - 1]
-    print('mpirun -np 4 ./build/Project4 1000000 %f %f 25 $L 1 0 pt_sub%d_L$L verbose' %(start, stop, i+1))
-
-    
-string = 
 string = '''#!/bin/bash
 
 L=40
@@ -73,18 +18,7 @@ while [ $L -lt 101 ]; do
 done
 '''
 for i in range(8):
-    with open('run_pt_sub%d.sh'%(i+1),'w') as outfile:
+    with open('shell/run_pt_sub%d_HD.sh'%(i+1),'w') as outfile:
         start, stop = a[i*len(a)/8], a[(i+1)*len(a)/8 - 1]
-        cmd = 'mpirun -np 4 ./build/Project4 1000000 %f %f 25 $L 1 0 pt_sub%d_L$L verbose' %(start, stop, i+1)
+        cmd = '/usr/lib64/openmpi/bin/mpirun -np 4 ./build/Project4 1500000 %f %f 24 $L 1 0 phase_transitions/pt_sub%d_L$L_HD verbose' %(start, stop, i+1)
         outfile.write(string.format(cmd))
-
-            
-    
- 
-    
-for i in range(8):
-    with open('run_pt_sub%d.sh'%(i+1),'w') as outfile:
-        start, stop = a[i*len(a)/8], a[(i+1)*len(a)/8 - 1]
-        cmd = 'mpirun -np 4 ./build/Project4 1000000 %f %f 25 $L 1 0 phase_transitions/pt_sub%d_L$L verbose' %(start, stop, i+1)
-        outfile.write(string.format(cmd))
-        

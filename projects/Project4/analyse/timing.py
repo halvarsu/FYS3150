d6ac9b5b0154ecc1772021bf14aafea1ab0c6ab2
Halvard Sutterud <halvard.sutterud@gmail.com>
2017-11-19 10:01:50 +0100

    timing update

--- projects/Project4/analyse/timing.py
+++ projects/Project4/analyse/timing.py
@@ -16,15 +16,15 @@ def timing(args):
     NMC = 10
     Tstart = 2
     Tstop = 2
-    nTemp = 100
+    nTemp = 1000
     L = 40
 
-    nTrials = 10
+    nTrials = 100
     trials = np.arange(nTrials)
     time = []
     time_parallel = []
     run = "mpirun -np 1 ./build/Project4 {} {} {} {} {} 1"
-    run_parallel = "mpirun -np 2 ./build/Project4 {} {} {} {} {} 1"
+    run_parallel = "mpirun -np 4 ./build/Project4 {} {} {} {} {} 1"
     for i in trials:
         out_parallel = subprocess.check_output(run_parallel.format( 
             NMC, Tstart, Tstop, nTemp, L), shell = True)
@@ -35,9 +35,17 @@ def timing(args):
         print(t, tp)
         time.append(t)
         time_parallel.append(tp)
+    avg_time = np.average(time)
+    avg_time_parallel = np.average(time_parallel)
+    print('Average time used 1 prosessor: {}s'.format(avg_time))
+    print('Average time used 4 prosessors: {}s'.format(avg_time_parallel))
     plt.scatter(trials, time)
     plt.scatter(trials, time_parallel)
-    plt.show()
+    plt.xlabel('Trial number')
+    plt.ylabel('Time used $[s]$')
+    plt.legend(['1 prosessor', '4 prosessors'])
+    plt.savefig('results/timing.pdf')
+    #plt.show()
     return
     
 

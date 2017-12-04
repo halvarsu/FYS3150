cf014598876050bde39dbe8bde3d61b2b6fd83ae
Halvard Sutterud <halvard.sutterud@gmail.com>
2017-12-04 13:55:00 +0100

    add fourth argument (steps) ++

--- projects/Project5/cpp/main.cpp
+++ projects/Project5/cpp/main.cpp
@@ -13,18 +13,20 @@ using namespace std;
 
 int main(int numberOfArguments, char **argumentList)
 {
-    double a = 4.3;
-    cout << floor(a / 2) << endl;
-    int numberOfUnitCells = 5;
+    int unitCellsPerDimention = 5;
     double initialTemperature = UnitConverter::temperatureFromSI(300.0); // measured in Kelvin
     double latticeConstant = UnitConverter::lengthFromAngstroms(5.26); // measured in angstroms
+    int numberOfTimeSteps = 10000;
 
     // If a first argument is provided, it is the number of unit cells
-    if(numberOfArguments > 1) numberOfUnitCells = atoi(argumentList[1]);
+    if(numberOfArguments > 1) unitCellsPerDimention = atoi(argumentList[1]);
     // If a second argument is provided, it is the initial temperature (measured in kelvin)
     if(numberOfArguments > 2) initialTemperature = UnitConverter::temperatureFromSI(atof(argumentList[2]));
     // If a third argument is provided, it is the lattice constant determining the density (measured in angstroms)
     if(numberOfArguments > 3) latticeConstant = UnitConverter::lengthFromAngstroms(atof(argumentList[3]));
+    // If a third argument is provided, it is the number of time steps.
+    if(numberOfArguments > 4) numberOfTimeSteps = atoi(argumentList[4]);
+
 
     double dt = UnitConverter::timeFromSI(1e-15); // Measured in seconds.
 
@@ -33,11 +35,12 @@ int main(int numberOfArguments, char **argumentList)
     cout << "One unit of time is " << UnitConverter::timeToSI(1.0) << " seconds" << endl;
     cout << "One unit of mass is " << UnitConverter::massToSI(1.0) << " kg" << endl;
     cout << "One unit of temperature is " << UnitConverter::temperatureToSI(1.0) << " K" << endl;
+    cout << "One unit of energy is " << UnitConverter::energyToSI(1.0) << " J" << endl;
 
     System system;
-    system.createFCCLattice(numberOfUnitCells, latticeConstant, initialTemperature);
+    system.createFCCLattice(unitCellsPerDimention, latticeConstant, initialTemperature);
     system.potential().setEpsilon(1.0);
-    system.potential().setSigma(1.0);
+    system.potential().setSigma(3.405);
 
     system.removeTotalMomentum();
 
@@ -50,7 +53,7 @@ int main(int numberOfArguments, char **argumentList)
             setw(20) << "KineticEnergy" <<
             setw(20) << "PotentialEnergy" <<
             setw(20) << "TotalEnergy" << endl;
-    for(int timestep=0; timestep<100000; timestep++) {
+    for(int timestep=0; timestep< numberOfTimeSteps; timestep++) {
         system.step(dt);
         statisticsSampler.sample(system);
         if( timestep % 100 == 0 ) {

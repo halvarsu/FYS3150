951d923ccfffb0865fe886d24d4e7b46cb7de1f0
halvarsu <halvard.sutterud@gmail.com>
2017-12-08 12:44:22 +0100

    no dumb file length param

--- projects/Project5/analyse/analyse.py
+++ projects/Project5/analyse/analyse.py
@@ -1,8 +1,10 @@
 import argparse
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib
+
 
 def main(args):
+    import matplotlib.pyplot as plt
     """Either runs all parts or just one"""
     parts = {1:part1, 2:part2}
     if args.part == 0:
@@ -14,25 +16,36 @@ def main(args):
 def part1(args):
     return
 
+def file_len(filename):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
+
+
 def part2(args):
     print("Part 2")
     import numpy as np
     import matplotlib.pyplot as plt
     import glob
-    file_length = 9999
+
     if args.temps == "all":
         all_folders = glob.glob('out/*')
         temps = [string.split('/')[-2].split('_')[-1] for string in all_folders]
+
     elif args.temps == "low":
-        temps = [15,40,45,60,90,150,200]
+        temps = list(range(25,625,25))
         all_folders = ["out/T_{}".format(T) for T in temps]
 
-    all_data = np.zeros(( len(all_folders),6,file_length))
+    # all_lengths =  [file_len(fname) for fname in all_folders]
+    all_data = []
+    # all_data = np.zeros(( len(all_folders),6,file_length))
 
     for i, folder in enumerate(all_folders):
         file = (folder+'/statistics.txt')
         a = np.loadtxt(file)
-        all_data[i] = a.T
+        all_data.append(a.T)
 
     fig, [ax1,ax2] = plt.subplots(2)
 
@@ -69,8 +82,11 @@ def get_args():
                         choices = [0,1,2,3])
     parser.add_argument('-T','--temps', default='all',
                         choices = ['all','low'])
+    parser.add_argument('--headless', help='Dont plot, only save', action = 'store_true')
     return parser.parse_args()
 
 if __name__=='__main__':
     args = get_args()
+    if args.headless:
+        matplotlib.use('Agg')
     main(args)
